5c66f665e90b3c9a37c9e3204c9523bc5fbf86e3
Alexandre Perrin <alex@kaworu.ch>
2013-08-26 13:38:06 +0200

    style(9)

--- t_toolkit.c
+++ t_toolkit.c
@@ -19,52 +19,50 @@
 bool
 t_yesno(const char *question)
 {
-	extern bool Yflag, Nflag;
-    char *endl;
-    char buffer[5]; /* strlen("yes\n\0") == 5 */
+	extern bool	Yflag, Nflag;
+	char		*endl;
+	char		buffer[5]; /* strlen("yes\n\0") == 5 */
 
-    for (;;) {
-        if (feof(stdin) && !Yflag && !Nflag)
-            return (false);
+	for (;;) {
+		if (feof(stdin) && !Yflag && !Nflag)
+			return (false);
 
-        (void)memset(buffer, '\0', sizeof(buffer));
+		(void)memset(buffer, '\0', sizeof(buffer));
 
-        if (question != NULL) {
-            (void)printf("%s? [y/n] ", question);
-            (void)fflush(stdout);
-        }
+		if (question != NULL) {
+			(void)printf("%s? [y/n] ", question);
+			(void)fflush(stdout);
+		}
 
-        if (Yflag) {
-            (void)printf("y\n");
-            return (true);
-        }
-        else if (Nflag) {
-            (void)printf("n\n");
-            return (false);
-        }
+		if (Yflag) {
+			(void)printf("y\n");
+			return (true);
+		} else if (Nflag) {
+			(void)printf("n\n");
+			return (false);
+		}
 
-        if (fgets(buffer, countof(buffer), stdin) == NULL) {
-            if (feof(stdin))
-                return (false);
-            else
-                err(errno, "fgets");
-        }
+		if (fgets(buffer, countof(buffer), stdin) == NULL) {
+			if (feof(stdin))
+				return (false);
+			else
+				err(errno, "fgets");
+		}
 
-        endl = strchr(buffer, '\n');
-        if (endl == NULL) {
-        /* buffer didn't receive EOL, must still be on stdin */
-            while (getc(stdin) != '\n' && !feof(stdin))
-                continue;
-        }
-        else {
-            *endl = '\0';
-            (void)t_strtolower(buffer);
-            if (strcmp(buffer, "n") == 0 || strcmp(buffer, "no") == 0)
-                return (false);
-            else if (strcmp(buffer, "y") == 0 || strcmp(buffer, "yes") == 0)
-                return (true);
-        }
-    }
+		endl = strchr(buffer, '\n');
+		if (endl == NULL) {
+			/* buffer didn't receive EOL, must still be on stdin */
+			while (getc(stdin) != '\n' && !feof(stdin))
+				continue;
+		} else {
+			*endl = '\0';
+			(void)t_strtolower(buffer);
+			if (strcmp(buffer, "n") == 0 || strcmp(buffer, "no") == 0)
+				return (false);
+			else if (strcmp(buffer, "y") == 0 || strcmp(buffer, "yes") == 0)
+				return (true);
+		}
+	}
 }
 
 
