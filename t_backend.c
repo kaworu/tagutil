8bd2c08d10d410975798913ca102c2bd1fa61b54
Alexandre Perrin <alex@kaworu.ch>
2013-09-07 16:20:09 +0200

    Removed backend setup (should be in t_ft<lib>_backend() routine). Removed the possibility to override the backend priority.

--- t_backend.c
+++ t_backend.c
@@ -5,15 +5,13 @@
  */
 #include "t_config.h"
 #include "t_toolkit.h"
+
 #include "t_backend.h"
 
 
-t__weak
-struct t_backend *t_ftflac_backend(void);
-t__weak
-struct t_backend *t_ftoggvorbis_backend(void);
-t__weak
-struct t_backend *t_generic_backend(void);
+struct t_backend	*t_ftflac_backend(void) t__weak;
+struct t_backend	*t_ftoggvorbis_backend(void) t__weak;
+struct t_backend	*t_ftgeneric_backend(void) t__weak;
 
 
 const struct t_backendQ *
@@ -26,57 +24,18 @@ t_all_backends(void)
 		struct t_backend *b, *b_temp;
 		char	*env;
 
-		/* add each available backend */
-#if defined(WITH_TAGLIB)
-		TAILQ_INSERT_HEAD(&bQ, t_generic_backend(), entries);
-#endif
-#if defined(WITH_OGGVORBIS)
-		TAILQ_INSERT_HEAD(&bQ, t_ftoggvorbis_backend(), entries);
-#endif
-#if defined(WITH_FLAC)
-		TAILQ_INSERT_HEAD(&bQ, t_ftflac_backend(), entries);
-#endif
-		/* change backend priority if requested by the env */
-		env = getenv("TAGUTIL_BACKEND");
-		if (env != NULL) {
-			char *start, *cur, *next;
-			struct t_backend *target;
+		/* add each available backend (order matter) */
+		if (t_ftflac_backend != NULL)
+			TAILQ_INSERT_TAIL(&bQ, t_ftflac_backend(), entries);
 
-			next = start = strdup(env);
-			while (next != NULL) {
-				next = strrchr(start, ',');
-				if (next == NULL)
-					cur = start;
-				else {
-					cur = next + 1;
-					*next = '\0';
-				}
-				target = NULL;
-				TAILQ_FOREACH(b, &bQ, entries) {
-					if (strcasecmp(b->libid, cur) == 0) {
-						target = b;
-						break;
-					}
-				}
-				if (target != NULL) {
-					TAILQ_REMOVE(&bQ, target, entries);
-					TAILQ_INSERT_HEAD(&bQ, target, entries);
-				}
-			}
-			free(start);
-		}
+		if (t_ftoggvorbis_backend != NULL)
+			TAILQ_INSERT_TAIL(&bQ, t_ftoggvorbis_backend(), entries);
 
-		/* call setup routine foreach backend needing it. */
-		TAILQ_FOREACH_SAFE(b, &bQ, entries, b_temp) {
-			if (b->setup != NULL) {
-				if (b->setup() != 0) {
-					warnx("backend %s setup failed.", b->libid);
-					TAILQ_REMOVE(&bQ, b, entries);
-				}
-			}
-		}
+		if (t_ftgeneric_backend != NULL)
+			TAILQ_INSERT_TAIL(&bQ, t_ftgeneric_backend(), entries);
 
 		initialized = 1;
 	}
+
 	return (&bQ);
 }
