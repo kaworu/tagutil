f4f5a8e6cd74fe2cb2d969ed3c32eef0411d0f11
Alexandre Perrin <alex@kaworu.ch>
2013-09-29 19:55:11 +0200

    Working on Ogg/Vorbis.

--- t_ftoggvorbis.c
+++ t_ftoggvorbis.c
@@ -34,8 +34,9 @@ static struct t_taglist	*t_ftoggvorbis_read(void *opaque);
 static int		 t_ftoggvorbis_write(void *opaque, const struct t_taglist *tlist);
 static void		 t_ftoggvorbis_clear(void *opaque);
 
-/* helper for t_ftoggvorbis_write() */
+/* helpers for t_ftoggvorbis_write() */
 static int		 fwrite_drain_func(void *fp, const char *data, int len);
+static int		 sbuf_write_ogg_page(struct sbuf *sb, ogg_page *p);
 
 
 struct t_backend *
@@ -126,25 +127,38 @@ t_ftoggvorbis_read(void *opaque)
 static int
 t_ftoggvorbis_write(void *opaque, const struct t_taglist *tlist)
 {
-	ogg_sync_state oy;
-	ogg_stream_state ios, oos;
-	ogg_page iog, oog;
-	vorbis_info vi;
-	ogg_packet op, my_vc_packet, *target;
-	vorbis_comment vc;
+	FILE             *fp_in  = NULL;  /* input file pointer */
+	FILE             *fp_out = NULL; /* output file pointer */
+	ogg_sync_state    oy_in;  /* sync and verify incoming physical bitstream */
+	ogg_stream_state  os_in;  /* take physical pages, weld into a logical
+	                             stream of packets */
+	ogg_stream_state  os_out; /* take physical pages, weld into a logical
+	                             stream of packets */
+	ogg_page          og_in;  /* one Ogg bitstream page. Vorbis packets are inside */
+	ogg_page          og_out; /* one Ogg bitstream page. Vorbis packets are inside */
+	ogg_packet        op_in;  /* one raw packet of data for decode */
+	ogg_packet        my_vc_packet; /* our custom packet containing vc_out */
+	vorbis_info       vi_in;  /* struct that stores all the static vorbis
+	                             bitstream settings */
+	vorbis_comment    vc_in;  /* struct that stores all the bitstream user
+	                             comment */
+	vorbis_comment    vc_out; /* struct that stores all the bitstream user
+	                             comment */
+	unsigned long     nstream_in; /* stream counter */
+	unsigned long     npage_in;   /* page counter */
+	unsigned long     npacket_in; /* packet counter */
+	unsigned long     bs;     /* blocksize of the current packet */
+	unsigned long     lastbs; /* blocksize of the last packet */
+	ogg_int64_t       granulepos; /* granulepos of the current page */
+	struct sbuf *sb = NULL;
+	char *tempfile  = NULL;
 	struct t_ftoggvorbis_data *data;
 	const struct t_tag *t;
-	struct t_error e;
-	struct sbuf *sb = NULL;
-	FILE *rfp = NULL, *wfp = NULL;
-	char *buf, *tmp = NULL;
-	ogg_int64_t granulepos = 0;
-	int npacket = 0, bs = 0, lastbs = 0;
 	enum {
-		BUILDING_VC_PACKET, SETUP, START_READING, STREAMS_INITIALIZED,
-		READING_HEADERS, READING_DATA, READING_DATA_NEED_FLUSH,
-		READING_DATA_NEED_PAGEOUT, E_O_S, WRITE_FINISH, RENAMING,
-		DONE_SUCCESS,
+		BUILDING_VC_PACKET, SETUP, B_O_S, START_READING,
+		STREAMS_INITIALIZED, READING_HEADERS, READING_DATA,
+		READING_DATA_NEED_FLUSH, READING_DATA_NEED_PAGEOUT, E_O_S,
+		WRITE_FINISH, RENAMING, DONE_SUCCESS,
 	} state;
 
 	assert_not_null(opaque);
@@ -152,211 +166,238 @@ t_ftoggvorbis_write(void *opaque, const struct t_taglist *tlist)
 	assert(data->libid == libid);
 
 	/*
-	 * The following is quite complicated. In order to modify the file's
-	 * tag, we have to rewrite the entire file. The Ogg container is divided
-	 * into "pages" and "packets" and the algorithm is to replace the SECOND
-	 * ogg packet (which contains vorbis comments) and copy ALL THE OTHERS.
+	 * In order to modify the file's tag, we have to rewrite the entire
+	 * file. The Ogg container is divided into "pages" and "packets" and the
+	 * algorithm is to replace the SECOND ogg packet (which contains vorbis
+	 * comments) and copy ALL THE OTHERS. See "Metadata workflow":
+	 * https://xiph.org/vorbis/doc/libvorbis/overview.html
 	 */
 
 	state = BUILDING_VC_PACKET;
-	vorbis_comment_init(&vc);
+	vorbis_comment_init(&vc_out);
 	TAILQ_FOREACH(t, tlist->tags, entries)
-		vorbis_comment_add_tag(&vc, t->key, t->val);
+		vorbis_comment_add_tag(&vc_out, t->key, t->val);
 	/* create the packet holding our vorbis_comment */
-	if (vorbis_commentheader_out(&vc, &my_vc_packet) != 0)
-		goto cleanup;
-	vorbis_comment_clear(&vc);
-	vorbis_comment_init(&vc);
+	if (vorbis_commentheader_out(&vc_out, &my_vc_packet) != 0)
+		goto cleanup_label;
+	vorbis_comment_clear(&vc_out);
 
 	state = SETUP;
 	/* open files & stuff */
-	if ((rfp = fopen(data->path, "r")) == NULL)
-		goto cleanup;
+	(void)ogg_sync_init(&oy_in); /* always return 0 */
+	if ((fp_in = fopen(data->path, "r")) == NULL)
+		goto cleanup_label;
 	if ((sb = sbuf_new(NULL, NULL, BUFSIZ + 1, SBUF_FIXEDLEN)) == NULL)
-		goto cleanup;
+		goto cleanup_label;
 	/* open the write file pointer */
-	if (asprintf(&tmp, "%s/.__%s_XXXXXX", t_dirname(data->path), getprogname()) < 0)
-		goto cleanup;
-	if (mkstemps(tmp, 0) == -1)
-		goto cleanup;
-	if ((wfp = fopen(tmp, "w")) == NULL)
-		goto cleanup;
-	sbuf_set_drain(sb, fwrite_drain_func, wfp);
-	vorbis_info_init(&vi);
-	(void)ogg_sync_init(&oy); /* always return 0 */
+	if (asprintf(&tempfile, "%s/.__%s_XXXXXX", t_dirname(data->path), getprogname()) < 0)
+		goto cleanup_label;
+	if (mkstemps(tempfile, 0) == -1)
+		goto cleanup_label;
+	if ((fp_out = fopen(tempfile, "w")) == NULL)
+		goto cleanup_label;
+	sbuf_set_drain(sb, fwrite_drain_func, fp_out);
+	lastbs = granulepos = 0;
+
+	nstream_in = 0;
+bos_label: /* beginning of a stream */
+	state = B_O_S;
+	nstream_in += 1;
+	npage_in = npacket_in = 0;
+	vorbis_info_init(&vi_in);
+	vorbis_comment_init(&vc_in);
 
 	state = START_READING;
 	/* main loop: read the input file into buf in order to sync pages out */
 	while (state != E_O_S) {
-		switch (ogg_sync_pageout(&oy, &iog)) {
+		switch (ogg_sync_pageout(&oy_in, &og_in)) {
 		case 0:  /* more data needed or an internal error occurred. */
 		case -1: /* stream has not yet captured sync (bytes were skipped). */
-			if (feof(rfp)) {
+			if (feof(fp_in)) {
 				if (state < READING_DATA)
-					goto cleanup;
+					goto cleanup_label;
+				/* There is no more data to read and we could
+				   not get a page so we're done here. */
 				state = E_O_S;
 			} else {
+				/* read more data and try again to get a page. */
+				char *buf;
 				size_t s;
-				if ((buf = ogg_sync_buffer(&oy, BUFSIZ)) == NULL)
-					goto cleanup;
-				if ((s = fread(buf, sizeof(char), BUFSIZ, rfp)) == -1)
-					goto cleanup;
-				if (ogg_sync_wrote(&oy, s) == -1)
-					goto cleanup;
+				/* get a buffer */
+				if ((buf = ogg_sync_buffer(&oy_in, BUFSIZ)) == NULL)
+					goto cleanup_label;
+				/* read a part of the file */
+				if ((s = fread(buf, sizeof(char), BUFSIZ, fp_in)) == -1)
+					goto cleanup_label;
+				/* tell ogg how much was read */
+				if (ogg_sync_wrote(&oy_in, s) == -1)
+					goto cleanup_label;
 			}
-			break;
-		case 1: /* a page was synced and returned. */
-			if (npacket == 0) {
-				/*
-				 * init both input and output streams with the
-				 * serialno of the first page
-				 */
-				if (ogg_stream_init(&ios, ogg_page_serialno(&iog)) == -1)
-					goto cleanup;
-				if (ogg_stream_init(&oos, ogg_page_serialno(&iog)) == -1) {
-					ogg_stream_clear(&ios);
-					goto cleanup;
-				}
-				state = STREAMS_INITIALIZED;
+			continue;
+		}
+		/* here ogg_sync_pageout() returned 1 and a page was sync'ed. */
+		if (++npage_in == 1) {
+			/* init both input and output streams with the serialno
+			   of the first page */
+			if (ogg_stream_init(&os_in, ogg_page_serialno(&og_in)) == -1)
+				goto cleanup_label;
+			if (ogg_stream_init(&os_out, ogg_page_serialno(&og_in)) == -1) {
+				ogg_stream_clear(&os_in);
+				goto cleanup_label;
 			}
+			state = STREAMS_INITIALIZED;
+		}
 
+		/* put the page in input stream, and then loop through each its
+		   packet(s) */
+		if (ogg_stream_pagein(&os_in, &og_in) == -1)
+			goto cleanup_label;
+		while (ogg_stream_packetout(&os_in, &op_in) == 1) {
+			ogg_packet *target;
 			/*
-			 * page in into the input stream, in order to fetch
-			 * ogg_packet from the page
+			 * This is where we really do what we mean to do: the
+			 * second packet is the commentheader packet, we replace
+			 * it with my_vc_packet if we're on the first stream.
 			 */
-			if (ogg_stream_pagein(&ios, &iog) == -1)
-				goto cleanup;
+			if (++npacket_in == 2 && nstream_in == 1)
+				target = &my_vc_packet;
+			else
+				target = &op_in;
 
-			while (ogg_stream_packetout(&ios, &op) == 1) {
+			if (npacket_in <= 3) {
 				/*
-				 * the second packet is the commentheader
-				 * packet, we replace it with my_vc_packet
+				 * The first three packets are header packets.
+				 * We use them to get the vorbis_info which
+				 * will be used later. vc_in will not be unused.
 				 */
-				target = (++npacket == 2 ? &my_vc_packet : &op);
+				if (vorbis_synthesis_headerin(&vi_in, &vc_in, &op_in) != 0)
+					goto cleanup_label;
+				/* force a flush after the third ogg_packet */
+				state = (npacket_in == 3 ? READING_DATA_NEED_FLUSH : READING_HEADERS);
+			} else {
+				/*
+				 * granulepos computation.
+				 *
+				 * The granulepos is stored into the *pages* and
+				 * is used by the codec to seek through the
+				 * bitstream.  Its value is codec dependent (in
+				 * the Vorbis case it is the number of samples
+				 * elapsed).
+				 *
+				 * The vorbis_packet_blocksize() actually
+				 * compute the number of sample that would be
+				 * stored by the packet (without decoding it).
+				 * This is the same formula as in vcedit example
+				 * from vorbis-tools.
+				 *
+				 * We use here the vorbis_info previously filled
+				 * when reading header packets.
+				 *
+				 * XXX: check if this is not a vorbis stream ?
+				 */
+				bs = vorbis_packet_blocksize(&vi_in, &op_in);
+				granulepos += (lastbs == 0 ? 0 : (bs + lastbs) / 4);
+				lastbs = bs;
 
-				/* insert the target packet into the output stream */
-				if (npacket <= 3) {
-					/*
-					 * the three first packet are used to
-					 * fill the vorbis info (used later for
-					 * granule computation)
-					 */
-					if (vorbis_synthesis_headerin(&vi, &vc, &op) != 0)
-						goto cleanup;
-					/* force a flush after the third ogg_packet */
-					state = (npacket == 3 ? READING_DATA_NEED_FLUSH : READING_HEADERS);
-				} else {
-					/* granule computation */
-					bs   = vorbis_packet_blocksize(&vi, &op);
-					granulepos += (lastbs == 0 ? 0 : (bs + lastbs) / 4);
-					lastbs   = bs;
-
-					/* save a page in the buffer a page if needed */
-					while (
-					    (state == READING_DATA_NEED_FLUSH && ogg_stream_flush(&oos, &oog)) ||
-					    (state == READING_DATA_NEED_PAGEOUT && ogg_stream_pageout(&oos, &oog))) {
-						(void)sbuf_bcat(sb, oog.header, oog.header_len);
-						(void)sbuf_bcat(sb, oog.body, oog.body_len);
-					}
-
-					/* NOTE: only the last packet ending on a page
-					   has granulepos set, see
-					   http://lists.xiph.org/pipermail/vorbis/2002-June/020453.html */
-					state = READING_DATA;
-					if (op.granulepos == -1) {
-						/* we set the granulepos for
-						 * this packet */
-						op.granulepos = granulepos;
-					} else if (granulepos > op.granulepos) {
-						/* we will force a page to be
-						   flushed */
-						granulepos = op.granulepos;
-						state = READING_DATA_NEED_FLUSH;
-					} else {
-						/* this packet has granulepos
-						   set, which hint that it is
-						   the last packet ending on
-						   this page, we do a pageout to
-						   output a page if needed. */
-						state = READING_DATA_NEED_PAGEOUT;
-					}
+				/* write page(s) if needed */
+				if (state == READING_DATA_NEED_FLUSH) {
+					while (ogg_stream_flush(&os_out, &og_out))
+						sbuf_write_ogg_page(sb, &og_out);
+				} else if (state == READING_DATA_NEED_PAGEOUT) {
+					while (ogg_stream_pageout(&os_out, &og_out))
+						sbuf_write_ogg_page(sb, &og_out);
 				}
-				if (ogg_stream_packetin(&oos, target) == -1)
-					goto cleanup;
+
+				/*
+				 * Decide wether we need to write a page based
+				 * on our granulepos computation. The -1 case is
+				 * very common because only the last packet of a
+				 * page has its granulepos set by the ogg layer
+				 * (which only store a granulepos per page), so
+				 * all the other have a value of -1 (we need to
+				 * set the granulepos for each packet though).
+				 *
+				 * The other cases logic are borrowed from
+				 * vcedit and I fail to see how granulepos could
+				 * mismatch because we don't alter the data
+				 * packet.
+				 */
+				state = READING_DATA;
+				if (op_in.granulepos == -1) {
+					op_in.granulepos = granulepos;
+				} else if (granulepos <= op_in.granulepos) {
+					state = READING_DATA_NEED_PAGEOUT;
+				} else /* if granulepos > op_in.granulepos */ {
+					state = READING_DATA_NEED_FLUSH;
+					granulepos = op_in.granulepos;
+				}
 			}
+			/* insert the target packet into the output stream */
+			if (ogg_stream_packetin(&os_out, target) == -1)
+				goto cleanup_label;
+		}
+		if (ogg_page_eos(&og_in)) {
+			/* og_in was the last page of the stream */
+			state = E_O_S;
 		}
 	}
 
 	/* forces remaining packets into a last page */
-	oos.e_o_s = 1;
-	while (ogg_stream_flush(&oos, &oog)) {
-		(void)sbuf_bcat(sb, oog.header, oog.header_len);
-		(void)sbuf_bcat(sb, oog.body, oog.body_len);
+	os_out.e_o_s = 1;
+	while (ogg_stream_flush(&os_out, &og_out))
+		sbuf_write_ogg_page(sb, &og_out);
+	/* ogg_page and ogg_packet structs always point to storage in libvorbis.
+	   They're never freed or manipulated directly */
+
+	/* check if we need to read another stream */
+	if (!feof(fp_in)) {
+		ogg_stream_clear(&os_in);
+		ogg_stream_clear(&os_out);
+		vorbis_comment_clear(&vc_in);
+		vorbis_info_clear(&vi_in);
+		goto bos_label;
+	} else {
+		(void)fclose(fp_in);
+		fp_in = NULL;
 	}
-	(void)fclose(rfp);
-	rfp = NULL;
 
 	state = WRITE_FINISH;
 	if (sbuf_finish(sb) == -1)
-		goto cleanup;
-	if (fclose(wfp) != 0)
-		goto cleanup;
-	wfp = NULL;
+		goto cleanup_label;
+	if (fclose(fp_out) != 0)
+		goto cleanup_label;
+	fp_out = NULL;
 
 	state = RENAMING;
-	if (rename(tmp, data->path) == -1)
-		goto cleanup;
+	if (rename(tempfile, data->path) == -1)
+		goto cleanup_label;
 
 	state = DONE_SUCCESS;
-
-cleanup:
-	t_error_init(&e);
-	switch (state) {
-	case DONE_SUCCESS: /* FALLTHROUGH */
-	case RENAMING:
-		if (state != DONE_SUCCESS && t_error_msg(&e) == NULL)
-			t_error_set(&e, "error while renaming temporary file");
-		/* FALLTHROUGH */
-	case WRITE_FINISH: /* FALLTHROUGH */
-	case E_O_S: /* FALLTHROUGH */
-	case READING_DATA_NEED_PAGEOUT: /* FALLTHROUGH */
-	case READING_DATA_NEED_FLUSH: /* FALLTHROUGH */
-	case READING_DATA: /* FALLTHROUGH */
-	case READING_HEADERS: /* FALLTHROUGH */
-	case STREAMS_INITIALIZED:
-		ogg_stream_clear(&ios);
-		ogg_stream_clear(&oos);
-		/* FALLTHROUGH */
-	case START_READING:
-		if (state != DONE_SUCCESS && t_error_msg(&e) == NULL)
-			t_error_set(&e, "error while reading or writting to temporary file");
-		ogg_sync_clear(&oy);
-		vorbis_info_clear(&vi);
-		/* FALLTHROUGH */
-	case SETUP:
-		if (state != DONE_SUCCESS && t_error_msg(&e) == NULL)
-			t_error_set(&e, "error while opening");
-		if (wfp != NULL)
-			(void)fclose(wfp);
-		if (tmp != NULL && eaccess(tmp, R_OK) != -1)
-			(void)unlink(tmp);
-		free(tmp);
-		if (sb != NULL)
-			sbuf_delete(sb);
-		if (rfp != NULL)
-			(void)fclose(rfp);
-		ogg_packet_clear(&my_vc_packet);
-		/* FALLTHROUGH */
-	case BUILDING_VC_PACKET:
-		if (state != DONE_SUCCESS && t_error_msg(&e) == NULL)
-			t_error_set(&e, "error while creating vorbis comment ogg_packet");
-		vorbis_comment_clear(&vc);
+cleanup_label:
+	if (state >= STREAMS_INITIALIZED) {
+		ogg_stream_clear(&os_in);
+		ogg_stream_clear(&os_out);
 	}
+	if (state >= START_READING) {
+		vorbis_comment_clear(&vc_in);
+		vorbis_info_clear(&vi_in);
+	}
+	ogg_sync_clear(&oy_in);
+	if (fp_out != NULL)
+		(void)fclose(fp_out);
+	if (tempfile != NULL && eaccess(tempfile, R_OK) != -1)
+		(void)unlink(tempfile);
+	free(tempfile);
+	if (sb != NULL)
+		sbuf_delete(sb);
+	if (fp_in != NULL)
+		(void)fclose(fp_in);
+	ogg_packet_clear(&my_vc_packet);
+	vorbis_comment_clear(&vc_out);
 
 	/*
-	 * XXX: at the moment, we ignore the error message
+	 * FIXME: at the moment, we don't warn about the problem.
 	 */
-	t_error_clear(&e);
 	return (state == DONE_SUCCESS ? 0 : -1);
 }
 
@@ -384,4 +425,19 @@ fwrite_drain_func(void *fp, const char *data, int len)
 		return (-1);
 	s = fwrite(data, 1, len, fp);
 	return (s == 0 ? -1 : s);
+}
+
+
+static int
+sbuf_write_ogg_page(struct sbuf *sb, ogg_page *og)
+{
+	int ret = 0;
+
+	assert_not_null(sb);
+	assert_not_null(og);
+
+	ret += sbuf_bcat(sb, og->header, og->header_len);
+	ret += sbuf_bcat(sb, og->body,   og->body_len);
+
+	return (ret == 0 ? 0 : -1);
 }
