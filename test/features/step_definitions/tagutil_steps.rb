e521f1fbe91f995fb2fab011754bd7039fc1cfda
Alexandre Perrin <alexandre.perrin@netoxygen.ch>
2016-05-26 17:48:26 +0200

    Finally fix the very old Ruby YAML parser "bug".
    Before this patch the YAML parsing (print) tests were failing because
    they did not work around some tricks from the Ruby YAML parser.

--- test/features/step_definitions/tagutil_steps.rb
+++ test/features/step_definitions/tagutil_steps.rb
@@ -12,10 +12,10 @@ Given(/^I have a music file (\w+)\.(mp3|ogg|flac)?$/) do |filename, ext|
     Tagutil.create_tune(filename, ext)
 end
 
+
 Given(/^I have a music file (\w+)\.(mp3|ogg|flac) tagged with:$/) do |filename, ext, tbl|
     Tagutil.create_tune(filename, ext, tags_from_cuke_table(tbl))
 end
-
 
 
 When(/^I run tagutil(.*)$/) do |params|
@@ -27,24 +27,44 @@ Then(/^I expect tagutil to succeed$/) do
     expect(@status.exitstatus).to eq(0)
 end
 
+
 Then(/^I expect tagutil to fail$/) do
     expect(@status.exitstatus).not_to eq(0)
 end
+
 
 Then(/^I should see "(.*?)"$/) do |text|
     expect(@output).to include(text)
 end
 
+
 Then(/^I should see the help about (.+)$/) do |section|
     expect(@output).to match(/^#{section}/m)
 end
+
 
 Then(/^I should see an empty (YAML|JSON) tag list$/) do |fmt|
     expect(Object.const_get(fmt).load(@output)).to be_empty
 end
 
-Then(/^I should see the (YAML|JSON) tag list:$/) do |fmt, tbl|
-    expect(Object.const_get(fmt).load(@output)).to eql(tags_from_cuke_table(tbl))
+
+Then(/^I should see the YAML tag list:$/) do |tbl|
+    tags = tags_from_cuke_table(tbl)
+
+    # XXX: the ruby YAML parser will convert any value that look like a number,
+    # so we need to do the same kind of trick on the cucumber table (otherwise
+    # the test will fail).
+    tags.each do |h|
+      h.update(h) { |k,v| if v =~ /^\d+$/ then v.to_i else v end }
+    end
+
+    expect(YAML.load(@output)).to eql(tags)
+end
+
+
+Then(/^I should see the JSON tag list:$/) do |tbl|
+    tags = tags_from_cuke_table(tbl)
+    expect(JSON.load(@output)).to eql(tags)
 end
 
 
