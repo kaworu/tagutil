42bbd667479028c011199ef2b54bfab085d74cbd
Alexandre Perrin <alexandre.perrin@netoxygen.ch>
2016-06-02 17:23:43 +0200

    cucumber: lets use a two space indent layout for ruby
    Like most of the rest of the world do.

--- test/features/step_definitions/tagutil_steps.rb
+++ test/features/step_definitions/tagutil_steps.rb
@@ -8,73 +8,73 @@ require 'expect'
 
 
 Given(/^I have a music file (\w+)\.(mp3|ogg|flac)?$/) do |filename, ext|
-    Tagutil.create_tune(filename, ext)
+  Tagutil.create_tune(filename, ext)
 end
 
 
 Given(/^I have a music file (\w+)\.(mp3|ogg|flac) tagged with:$/) do |filename, ext, tbl|
-    Tagutil.create_tune(filename, ext, tags_from_cuke_table(tbl))
+  Tagutil.create_tune(filename, ext, tags_from_cuke_table(tbl))
 end
 
 
 When(/^I run tagutil(.*)$/) do |params|
-    @output, @status = Tagutil.run(argv: params)
+  @output, @status = Tagutil.run(argv: params)
 end
 
 
 Then(/^I expect tagutil to succeed$/) do
-    expect(@status.exitstatus).to eq(0)
+  expect(@status.exitstatus).to eq(0)
 end
 
 
 Then(/^I expect tagutil to fail$/) do
-    expect(@status.exitstatus).not_to eq(0)
+  expect(@status.exitstatus).not_to eq(0)
 end
 
 
 Then(/^I should see "(.*?)"$/) do |text|
-    expect(@output).to include(text)
+  expect(@output).to include(text)
 end
 
 
 Then(/^I should see the help about (.+)$/) do |section|
-    expect(@output).to match(/^#{section}/m)
+  expect(@output).to match(/^#{section}/m)
 end
 
 
 Then(/^I should see an empty (YAML|JSON) tag list$/) do |fmt|
-    expect(Object.const_get(fmt).load(@output)).to be_empty
+  expect(Object.const_get(fmt).load(@output)).to be_empty
 end
 
 
 Then(/^I should see the YAML tag list:$/) do |tbl|
-    tags = tags_from_cuke_table(tbl)
+  tags = tags_from_cuke_table(tbl)
 
-    # XXX: the ruby YAML parser will convert any value that look like a number,
-    # so we need to do the same kind of trick on the cucumber table (otherwise
-    # the test will fail).
-    tags.each do |h|
-      h.update(h) { |k,v| if v =~ /^\d+$/ then v.to_i else v end }
-    end
+  # XXX: the ruby YAML parser will convert any value that look like a number,
+  # so we need to do the same kind of trick on the cucumber table (otherwise
+  # the test will fail).
+  tags.each do |h|
+    h.update(h) { |k,v| if v =~ /^\d+$/ then v.to_i else v end }
+  end
 
-    expect(YAML.load(@output)).to eql(tags)
+  expect(YAML.load(@output)).to eql(tags)
 end
 
 
 Then(/^I should see the JSON tag list:$/) do |tbl|
-    tags = tags_from_cuke_table(tbl)
-    expect(JSON.load(@output)).to eql(tags)
+  tags = tags_from_cuke_table(tbl)
+  expect(JSON.load(@output)).to eql(tags)
 end
 
 
 AfterConfiguration do
-    Tagutil.build
+  Tagutil.build
 end
 
 Before do
-    Tagutil.setup
+  Tagutil.setup
 end
 
 After do
-    Tagutil.teardown
+  Tagutil.teardown
 end
--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -8,71 +8,71 @@ require 'yaml'
 
 
 module Tagutil
-    ProjectRoot = File.join(File.dirname(__FILE__), '..', '..', '..')
-    Executable  = File.join(ProjectRoot, 'build', 'tagutil')
-    @tmpdir     = nil # temporary test directory
-    @blankfiles = []  # music test files
-    @tmpfiles   = []  # generated files
+  ProjectRoot = File.join(File.dirname(__FILE__), '..', '..', '..')
+  Executable  = File.join(ProjectRoot, 'build', 'tagutil')
+  @tmpdir     = nil # temporary test directory
+  @blankfiles = []  # music test files
+  @tmpfiles   = []  # generated files
 
-    # build tagutil iff the executable does not exist
-    def self.build
-        self.force_build unless File.exist?(Executable)
-    end
+  # build tagutil iff the executable does not exist
+  def self.build
+    self.force_build unless File.exist?(Executable)
+  end
 
-    # build tagutil
-    def self.force_build
-        STDOUT.print "===> running a debug build..."
-        STDOUT.flush
-        output, status = Open3.capture2e("make -C #{ProjectRoot}")
-        raise RuntimeException.new('Could not build tagutil') unless status.success?
-        STDOUT.puts "done."
-        STDOUT.flush
-    end
+  # build tagutil
+  def self.force_build
+    STDOUT.print "===> running a debug build..."
+    STDOUT.flush
+    output, status = Open3.capture2e("make -C #{ProjectRoot}")
+    raise RuntimeException.new('Could not build tagutil') unless status.success?
+    STDOUT.puts "done."
+    STDOUT.flush
+  end
 
-    # called by a Before hook
-    def self.setup
-        self.build
-        @tmpdir ||= Dir.mktmpdir('tagutil-cucumber-')
-        Dir.chdir @tmpdir
-        @blankfiles = Dir[File.join(ProjectRoot, 'test', 'files', '*')] if @blankfiles.empty?
-    end
+  # called by a Before hook
+  def self.setup
+    self.build
+    @tmpdir ||= Dir.mktmpdir('tagutil-cucumber-')
+    Dir.chdir @tmpdir
+    @blankfiles = Dir[File.join(ProjectRoot, 'test', 'files', '*')] if @blankfiles.empty?
+  end
 
-    def self.teardown
-        while (victim = @tmpfiles.pop) do
-            FileUtils.rm(victim, force: true)
-        end
+  def self.teardown
+    while (victim = @tmpfiles.pop) do
+      FileUtils.rm(victim, force: true)
     end
+  end
 
-    # create a music file by copying the blank file matching the requested
-    # extension.
-    def self.create_tune(filename, ext, tags=nil)
-        tune  = "#{filename}.#{ext}"
-        blank = @blankfiles.select { |f| f =~ /\.#{ext}$/ }.first
-        raise ArgumentError.new "#{ext}: bad file extension" unless blank
-        FileUtils.cp blank, tune
-        if tags
-            data = "#{tags.to_yaml}\n"
-            output, status = Open3.capture2e("#{Executable} load:- #{tune}", stdin_data: data)
-            raise RuntimeError.new(output) unless status.success? and output.empty?
-        end
-        @tmpfiles << tune
+  # create a music file by copying the blank file matching the requested
+  # extension.
+  def self.create_tune(filename, ext, tags=nil)
+    tune  = "#{filename}.#{ext}"
+    blank = @blankfiles.select { |f| f =~ /\.#{ext}$/ }.first
+    raise ArgumentError.new "#{ext}: bad file extension" unless blank
+    FileUtils.cp blank, tune
+    if tags
+      data = "#{tags.to_yaml}\n"
+      output, status = Open3.capture2e("#{Executable} load:- #{tune}", stdin_data: data)
+      raise RuntimeError.new(output) unless status.success? and output.empty?
     end
+    @tmpfiles << tune
+  end
 
-    def self.run(env: {}, argv: "")
-      cmd = "#{Executable} #{argv}"
-      Open3.capture2e(env, cmd)
-    end
+  def self.run(env: {}, argv: "")
+    cmd = "#{Executable} #{argv}"
+    Open3.capture2e(env, cmd)
+  end
 
-    class World
-        def tags_from_cuke_table tbl
-            tbl.raw.map do |x|
-                {x.first => x.last}
-            end
-        end
+  class World
+    def tags_from_cuke_table tbl
+      tbl.raw.map do |x|
+        {x.first => x.last}
+      end
     end
+  end
 end
 
 
 World do
-    Tagutil::World.new
+  Tagutil::World.new
 end
